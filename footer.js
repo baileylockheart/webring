a513e8084ec13ec5f522e5a026030d8e63b70ef5
Bailey Lockheart <baileylockheart@gmail.com>
2024-12-28 15:24:12 -0800

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,8 @@
+document.getElementById("footer").innerHTML = `
+<a href="https://open.spotify.com/artist/0gZWd2jEBp1DloD73xiL8L"><i class="fa-brands fa-spotify"></i></a>&nbsp;&nbsp;
+<a href="https://www.youtube.com/@bailey_lockheart"><i class="fa-brands fa-youtube"></i></a>&nbsp;&nbsp;
+<a href="https://baileylockheart.tumblr.com"><i class="fa-brands fa-tumblr"></i></a>&nbsp;&nbsp;
+<a href="https://www.threads.net/@bailey_lockheart"><i class="fa-brands fa-threads"></i></a>&nbsp;&nbsp;
+<a href="https://github.com/baileylockheart"><i class="fa-brands fa-github"></i></a>&nbsp;&nbsp;
+<a href="https://codepen.io/lockheartlove143"><i class="fa-brands fa-codepen"></i></a>
+`;
